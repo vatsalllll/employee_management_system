7597ea544d4d7d1ebb352c3250cfe64baf5493f2
vatsal <vatsal3064@gmail.com>
2024-11-23 11:09:12 +0530

    updated the necessary files and implemented all features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import {React} from 'react'
-import EmployeeDashBoard from './components/DashBoard/EmployeedashBoard'
-import AdminDashBoard from './components/DashBoard/AdminDashBoard';
-function App(){
-  return(
+import React, { useContext, useEffect, useState } from 'react'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from '../components/Dashboard/EmployeeDashBoard'
+import AdminDashboard from '../components/Dashboard/AdminDashBoard'
+import { AuthContext } from './context/AuthProvider'
+
+const App = () => {
+
+  const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  const [userData,SetUserData] = useContext(AuthContext)
+
+  useEffect(()=>{
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
+    if(loggedInUser){
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    }
+
+  },[])
+
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+      }
+    }
+    else {
+      alert("Invalid Credentials")
+    }
+  }
+
+
+
+  return (
     <>
-      {/* <Login /> */}
-      {/* <EmployeeDashBoard/> */}
-      <AdminDashBoard />
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
     </>
   )
 }
-export default App;
+
+export default App
